7885fc0beb412870f05397de8b9b35c000e153ad
超绝美味家乐堡 <2359376088@qq.com>
2024-12-03 16:18:43 +0800

    dark mode

--- src/pages/detail.vue
+++ src/pages/detail.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { useColorMode } from '@vueuse/core';
+const mode = useColorMode();
+</script>
 <template>
-  <div class="detail">detail</div>
+  <div class="flex items-center justify-center h-svh w-full gap-2 bg-blue dark:bg-slate-950">
+    <button @click="mode = 'light'" class="p-2 bg-amber text-sm ">默认</button>
+    <button @click="mode = 'dark'" class="p-2 bg-blue text-sm ">深色</button>
+  </div>
 </template>
